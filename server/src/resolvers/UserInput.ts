6146ece91a3477afedde4c74e64ad37247a7cdb6
Ghazi Kadardar <ghazikadardar@gmail.com>
2021-10-05 21:13:37 +0200

    added missing files

---
+++ server/src/resolvers/UserInput.ts
@@ -0,0 +1,13 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class UserInput {
+  @Field()
+  email: string;
+
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
